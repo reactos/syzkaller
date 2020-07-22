8828e0a623702d491c32a3bef1eb6704a4e42631
Suraj K Suresh <kssuraj15@gmail.com>
2020-07-22 14:21:05 +0530

    fix copyrigth and add name to CONTRIBUTORS

--- executor/common.h
+++ executor/common.h
@@ -1,4 +1,4 @@
-// Copyright 2016 syzkaller project authors. All rights reserved.
+// Copyright 2020 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
 // This file is shared between executor and csource package.
